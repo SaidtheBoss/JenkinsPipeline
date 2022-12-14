15f68cad287975a2ad64f7d2bd0e5a1473136c7e
Theboss <saidabbos77@gmail.com>
2022-12-13 20:32:54 -0500

    Create vars.tf

---
+++ terraform/vars.tf
@@ -0,0 +1,32 @@
+variable "default_region" {
+  type = string
+  default = "us-east-1"
+}
+##---- vpc vars -----
+variable "vpc_cird" {
+  type = string
+  default = "124.10.0.0/16"
+}
+
+variable "sub_cidr_pub" {
+  type    = string
+  default = "124.10.1.0/24"
+}
+variable "sub_cidr_pub2" {
+  type    = string
+  default = "124.10.2.0/24"
+}
+
+
+variable "cidr_all_ips" {
+  type    = string
+  default = "0.0.0.0/0"
+}
+variable "sub_az_1" {
+  type    = string
+  default = "us-east-1b"
+}
+variable "sub_az_2" {
+  type    = string
+  default = "us-east-1a"
+}
