ef127b459ad8ca3f43acac345f9df358153f9f43
Theboss <saidabbos77@gmail.com>
2022-12-15 22:54:28 -0500

    update

--- terraform/run_images.sh
+++ terraform/run_images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum install -y httpd
 mkdir /var/www/html/images
-echo "<h1> $(hostname -I) This is images site</h1>" > /var/www/html/images/index.html
+echo "<img src="https://i.redd.it/scupjyjwyqs11.jpg">" > /var/www/html/images/index.html
 systemctl start httpd
 systemctl enable httpd
