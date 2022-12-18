54cc9220c94b615947e7684f40be18ea4d47da6b
Theboss <saidabbos77@gmail.com>
2022-12-18 12:12:29 -0500

    Delete run_images.sh

--- terraform/run_images.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-yum install -y httpd
-mkdir /var/www/html/images
-echo "<img src="https://i.redd.it/scupjyjwyqs11.jpg">" > /var/www/html/images/index.html
-systemctl start httpd
-systemctl enable httpd
