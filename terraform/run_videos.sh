d97c2cfdfef2dedbe20e0239bcc548dda685b9d9
Theboss <saidabbos77@gmail.com>
2022-12-18 12:12:50 -0500

    Delete run_videos.sh

--- terraform/run_videos.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-yum install -y httpd
-mkdir /var/www/html/videos
-echo "<iframe width="420" height="345" src="https://www.youtube.com/embed/KZckZ6ZIVTU?autoplay=1&mute=1">" > /var/www/html/videos/index.html
-systemctl start httpd
-systemctl enable httpd
